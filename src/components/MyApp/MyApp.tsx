abc2396a0e2c0657afe74a11a249caaf3a15d103
ermek <ermaaskarbekov@gmail.com>
2022-11-15 15:51:59 +0600

    Создал компоненты которые буду использовать в проекте

---
+++ src/components/MyApp/MyApp.tsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+const MyApp = () => {
+
+    const [showModal, setShowModal] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+
+
+
+
+
+    return (
+<div></div>
+    );
+};
+
+export default MyApp;
